18d52fde1201bb93eec120dc76f28dfe0c7ee88d
Elsie Yi <yi.elsie@gmail.com>
2023-10-16 12:25:02 -0700

    Initial edits

---
+++ MalpracticeProj.R
@@ -0,0 +1,22 @@
+# Medical Malpractice
+
+Nearly [50% of physicians age 55 and older have reported being sued ](https://www.ama-assn.org/practice-management/sustainability/1-3-physicians-has-been-sued-age-55-1-2-hit-suit) according to the American Medical Association and in [2022, 31% of physicians have been sued](https://www.ama-assn.org/practice-management/sustainability/medical-liability-market-research). 
+With the prevalence of these malpractice suits I was interested to see if there is any association between the amount received by the patient and whether they had a private attorney, what type of insurance they had (private, uninsured, or government), severity of damage, as well as age and gender of patient. 
+I am also curious to see if there is a greater proportion of suits given the type of specialty of the physician.
+
+
+## Dataset
+
+Data is from [Kaggle](https://www.kaggle.com/datasets/gabrielsantello/medical-malpractice-insurance-dataset/) and the analysis is completed in R. 
+Visualization was completed with Tableau.
+mm <- read.csv("/Users/elsieyi/Documents/Projects/Kaggle/Medical Malpractice/medicalmalpractice.csv")
+library(ggplot2)
+
+## Initial notes regarding data
+#Malpractice suit amount ranges from 
+min(mm$Amount)-max(mm$Amount)
+
+## Analyses
+### Private Attorney
+pa <- glm(Amount ~ Private.Attorney + Insurance + Private.Attorney*Insurance, data = mm)
+summary(pa)
